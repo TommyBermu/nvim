5c3da9834868bf7d4f7b7a1d3028eddfc8c65c81
Tomas Bermudez <bermudeztomas06@gmail.com>
2025-09-25 14:33:51 -0500

    añadido un codigo para quitar las advertencias de obsolecencia de lsp

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
+-- Silenciar advertencias de deprecación
+vim.notify = function(msg, log_level, opts)
+    if msg and msg:find("deprecated") then
+        return
+    end
+    return require("vim.notify")(msg, log_level, opts)
+end
+
 require("default") -- la carpeta default
