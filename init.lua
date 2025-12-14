26b66b4f773664ae5ab507f2193d54c6436a4b71
TommyBermu <bermudeztomas06@gmail.com>
2025-12-13 22:04:01 -0500

    modificado nada importante

--- init.lua
+++ init.lua
@@ -1,16 +1,1 @@
--- Silenciar advertencias de deprecación
-local original_notify = vim.notify
-vim.notify = function(msg, log_level, opts)
-    if msg and msg:find("deprecated") then
-        return
-    end
-    original_notify(msg, log_level, opts)
-end
--- Silenciar advertencias de deprecación
--- vim.notify = function(msg, log_level, opts)
---     if msg and msg:find("deprecated") then
---         return
---     end
---     print(msg)  -- Solo imprimir en lugar de notificar
--- end
 require("default") -- la carpeta default
